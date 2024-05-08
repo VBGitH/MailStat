d0f3acad8720f2a21d02168ed4ead649479c4e7e
VBGitH <vb@pcs.ru>
2024-05-08 12:51:04 +0300

    pass

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/', express.static('public'))
 
 //http://localhost:3030/api/mail/open?LidGuid=123&LetterGuid=321
 //https://mail-stat.vercel.app/api/mail/open?LidGuid=123&LetterGuid=321
-
+//событие открытия письма
 app.get('/api/mail/open', async (req, res) => {
 
   /* ---- запись БД ---- */
@@ -36,24 +36,38 @@ app.get('/api/mail/open', async (req, res) => {
   res.statusCode = 200
   res.sendFile(pathPoint)
 
-  })
+})
 
-  app.get('/api/mail/unsubscribe', async (req, res) => {
+//https://mail-stat.vercel.app/api/mail/unsubscribe?LidGuid=123&LetterGuid=321
+//событие отписки от рассылки
+app.get('/api/mail/unsubscribe', async (req, res) => {
 
-    /* ---- запись БД ---- */
-    const recordSet = new mailAction()
-    recordSet.action = 'unsubscribe'
-    recordSet.date = new Date()
-    recordSet.guidObg = req.query.LidGuid
-    recordSet.guidLetter = req.query.LetterGuid
-    recordSet.addInf = ''
-  
-    await recordSet.save()
-  
-    res.statusCode = 200
-    res.end('Выполнено отключение от рассылки')
-  
-    })  
+  /* ---- запись БД ---- */
+  const recordSet = new mailAction()
+  recordSet.action = 'unsubscribe'
+  recordSet.date = new Date()
+  recordSet.guidObg = req.query.LidGuid
+  recordSet.guidLetter = req.query.LetterGuid
+  recordSet.addInf = ''
+
+  await recordSet.save()
+
+  res.statusCode = 200
+  res.end('Выполнено отключение от рассылки')
+
+})
+
+//синхронизация с УНФ (пароль передаем в запросе для чтения)
+//https://mail-stat.vercel.app/api/getupdates/?pass=adter7re54556njfdhgfdgg8756546
+app.get('/api/getupdates', async (req, res) => {
+
+  if(req.query.pass == process.env.PASSWORD_READ){
+      res.end('success')
+    }else{
+      res.end('denide')
+    }
+
+})    
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
