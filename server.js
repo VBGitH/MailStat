5ad3ece226682fba0afff06fb74ceef8436bd429
VBGitH <vb@pcs.ru>
2024-05-08 07:37:59 +0300

    db

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const app = express()
 const port = 3030
 //const pathPoint = __dirname + '\\public\\visible_point.png'
 const pathPoint = __dirname + '/public/visible_point.png'
-
+require('dotenv').config()
 const mongoose = require('mongoose')
 //mongoose.connect('mongodb://127.0.0.1:27017/meteoDB')
 mongoose.connect(process.env.MONGODB_URI)
 const mailAction = mongoose.model('mailAction', { 
   action          : String,
-  date            : String,
+  date            : Date,
   guidObg         : String,
   guidLetter      : String,
   addInf          : String, 
@@ -17,17 +17,26 @@ const mailAction = mongoose.model('mailAction', {
 
 app.use('/', express.static('public'))
 
+//http://localhost:3030/api/mail/open?LidGuid=123&LetterGuid=321
+
 app.get('/api/mail/open', async (req, res) => {
-    
+
+  /* ---- запись БД ---- */
+  const recordSet = new mailAction()
+  recordSet.action = 'readMail'
+  recordSet.date = new Date()
+  recordSet.guidObg = req.query.LidGuid
+  recordSet.guidLetter = req.query.LetterGuid
+  recordSet.addInf = ''
+
+  await recordSet.save()
+
+  /* ---- возвращаем картинку ---- */
   res.statusCode = 200
   res.sendFile(pathPoint)
-
-  //res.json({
-  //  'path' : pathPoint
-  //})
 
   })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  //console.log(`Example app listening on port http://localhost:${port}`)
 })
